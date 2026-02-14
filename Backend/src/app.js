e3c6dc6616c776404ff617b01822995d7854c570
Sushmit panday Panday <pandaysushmitpanday@gmail.com>
2026-02-14 15:35:12 +0530

    footer

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,8 @@ const connectDB = require('./database/db');
 const Venue = require('./database/models/venue.model');
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(CookieParser());
 app.use(cors({
     origin: true,
